d45eaab25792fbecb9d2d46ea1e28b2092ce1669
Ingo Stramm <ingostramm@gmail.com>
2024-11-25 14:26:33 -0300

    feat shortcode product percentage discount

--- bazar.php
+++ bazar.php
@@ -4,7 +4,7 @@
  * Plugin Name: Bazar
  * Plugin URI: https://agencialaf.com
  * Description: Plugin do site Bazar.
- * Version: 0.0.2
+ * Version: 0.0.3
  * Author: Ingo Stramm
  * Text Domain: bazar
  * License: GPLv2
@@ -26,6 +26,7 @@ function bazar_debug($debug)
 // require_once 'classes/classes.php';
 require_once 'scripts.php';
 require_once 'woocommerce.php';
+require_once 'shortcodes.php';
 
 require 'plugin-update-checker-4.10/plugin-update-checker.php';
 $updateChecker = Puc_v4_Factory::buildUpdateChecker(
---
+++ shortcodes.php
@@ -0,0 +1,36 @@
+<?php
+
+add_shortcode('bazar_pct_price', 'bazar_discount_product_percentage_price');
+
+/**
+ * bazar_discount_product_percentage_price
+ *
+ * @param  array $atts
+ * @return string
+ */
+function bazar_discount_product_percentage_price($atts)
+{
+    global $product;
+    if (!$product) {
+        return;
+    }
+    if (!$product->is_on_sale()) {
+        return;
+    }
+    if ($product->is_type('simple')) {
+        $max_percentage = (($product->get_regular_price() - $product->get_sale_price()) / $product->get_regular_price()) * 100;
+    } elseif ($product->is_type('variable')) {
+        $max_percentage = 0;
+        foreach ($product->get_children() as $child_id) {
+            $variation = wc_get_product($child_id);
+            $price = $variation->get_regular_price();
+            $sale = $variation->get_sale_price();
+            if ($price != 0 && ! empty($sale)) $percentage = ($price - $sale) / $price * 100;
+            if ($percentage > $max_percentage) {
+                $max_percentage = $percentage;
+            }
+        }
+    }
+    $max_percentage = $max_percentage ? round($max_percentage) .  '% OFF' : null;
+    return $max_percentage;
+}
